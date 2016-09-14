c691579699e0db6883bbb6d3775c217774bcd60a
zack <patterza@mail.gvsu.edu>
2016-09-14 22:56:27 +0000

    initial commit

---
+++ unit.py
@@ -0,0 +1,10 @@
+#Old C Code
+#https://github.com/TimJung/Passageway/blob/master/pi/pi.c
+
+#CHIP GPIO Library
+#https://github.com/xtacocorex/CHIP_IO
+
+def main():
+    print 'Hello World'
+
+main()
