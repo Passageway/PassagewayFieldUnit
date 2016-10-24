a667b11a811a26203f4377ab4f804f8066425597
Zack Patterson <zpattdev@gmail.com>
2016-10-24 21:27:28 +0000

    testing update ip program

--- push_ip.py
+++ push_ip.py
@@ -1,10 +1,61 @@
 import socket
 import fcntl
 import struct
+import pyrebase
+
+db = None
+mac = None
+ip = None
 
 def main():
-    print(get_ip_address("wlan0"))
     
+    global ip
+    ip = get_ip_address("wlan0")
+    print("IP Address obtained: " + ip)
+    
+    firebase = firebase_setup()
+    global db
+    db = firebase.database()
+    print("Firebase is setup")
+  
+def update_ip():    
+    found = False
+
+    units = db.child("units").get()
+    for unit in units.each():
+        dict = unit.val()
+        if (dict['cid'] == mac):
+            print("We found our unit: " + str(dict['cid']))
+            #update firebase entry
+            #data = unit.val()
+            data = {"ip": ip}
+            db.child("units").child(unit.key()).update(data)
+            found = True
+            break
+    if not found:
+        #push to firebase
+        data = {"building": "temp",
+            "ip": ip,
+            "cid": mac,
+            "direction": 0,
+            "floor": 1,
+            "lat": 0,
+            "lon": 0,
+            "name": "temp",
+            "wing": "temp"}
+        db.child("units").push(data)
+        print("Unit not found. Pushing new unit: " + str(mac))
+ 
+def firebase_setup():
+    txt = open("apiKey.txt")
+    config = {
+        "apiKey": txt.read(),
+        "authDomain": "project-8002914138129972242.firebaseapp.com",
+        "databaseURL": "https://project-8002914138129972242.firebaseio.com",
+        "storageBucket": "gs://project-8002914138129972242.appspot.com",
+        "serviceAccount": "serviceCredentials.json"
+    }
+    return pyrebase.initialize_app(config)
 
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
